6478c598998e97d8497084bdb68afa529f86b545
Parker Wilson <parker@tract.us>
2020-10-26 10:54:44 -0600

    Make Presenter and Task for CreatePosting feature. Still need to do PostingsFeature

--- app/src/main/java/com/example/roommatefinder/model/service/CreatePostingService.java
+++ app/src/main/java/com/example/roommatefinder/model/service/CreatePostingService.java
@@ -1,17 +1,18 @@
 package com.example.roommatefinder.model.service;
 
 import com.example.roommatefinder.model.service.request.CreatePostingRequest;
-import com.example.roommatefinder.model.service.request.LoginRequest;
 import com.example.roommatefinder.net.ServerFacade;
-import com.example.roommatefinder.net.asynctasks.LoginTaskFacade;
+import com.example.roommatefinder.net.asynctasks.CreatePostingTaskFacade;
 
 import java.io.IOException;
 
-public class CreatePostService {
-    public void addPosting(CreatePostingRequest request, LoginTaskFacade.Observer observer) throws IOException {
+public class CreatePostingService {
+    public void createPosting(CreatePostingRequest request, CreatePostingTaskFacade.Observer observer) throws IOException {
         //TODO: implement and call function that creates a posting in serviceFacade.
         //getServerFacade().login(request, observer);
     }
 
-    private ServerFacade getServerFacade(){return new ServerFacade();}
+    private ServerFacade getServerFacade() {
+        return new ServerFacade();
+    }
 }
