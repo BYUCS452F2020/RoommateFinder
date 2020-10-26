71dfba966eaa8f271c8962cb909349298095f1c5
Travisgra13 <39746960+Travisgra13@users.noreply.github.com>
2020-10-25 20:09:28 -0600

    Implemented Login

--- app/src/main/java/com/example/roommatefinder/net/DBDAO/AuthTokenTable.java
+++ app/src/main/java/com/example/roommatefinder/net/DBDAO/AuthTokenTable.java
@@ -1,5 +1,6 @@
 package com.example.roommatefinder.net.DBDAO;
 
+import com.example.roommatefinder.Utils.RandomAuthTokenGenerator;
 import com.example.roommatefinder.model.AuthToken;
 import com.example.roommatefinder.model.User;
 import com.example.roommatefinder.model.service.request.LoginRequest;
@@ -13,8 +14,20 @@ import java.sql.SQLException;
 public class AuthTokenTable {
     public AuthToken Create(LoginRequest request) {
         //create authToken
-         new SQLAccess().createNewDatabase("test");
-        return new AuthToken("Token");
+        boolean success = false;
+        String generatedToken = null;
+        try {
+            generatedToken = new RandomAuthTokenGenerator().generateAuthToken();
+            success = SQLAccess.addEntryToAuthTokenTable(request, generatedToken);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (success) {
+            return new AuthToken(generatedToken);
+        }
+        else {
+            return null;
+        }
     }
 
     public Boolean Update(LoginRequest request) {
--- app/src/main/java/com/example/roommatefinder/net/SQLAccess.java
+++ app/src/main/java/com/example/roommatefinder/net/SQLAccess.java
@@ -4,6 +4,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.StrictMode;
 
+import com.example.roommatefinder.model.service.request.LoginRequest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.Date;
@@ -13,24 +17,24 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 
 public class SQLAccess {
-    private static String ip = "192.168.1.4";
+    private static String ip = "192.168.1.4"; //ip address of sql server
     private static String port = "1433";
     private static String ClassName = "net.sourceforge.jtds.jdbc.Driver";
     private static String database = "RoommateFinderDB";
     private static String username = "test";
     private static String password = "test";
     private static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+"/"+database;
-    private Connection conn = null;
+    private static Connection conn = null;
 
-        public void createNewDatabase(String fileName) {
+        private static void establishConnection() {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             try {
                 Class.forName(ClassName);
                 conn = DriverManager.getConnection(url, username, password);
-                database();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             } catch (SQLException e) {
@@ -38,14 +42,28 @@ public class SQLAccess {
             }
         }
 
-        private void database() throws SQLException {
+        public static boolean addEntryToAuthTokenTable(LoginRequest request, String generatedToken) throws SQLException {
+            establishConnection();
             if (conn != null) {
                 Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery("Select * FROM Test;");
-                while(resultSet.next()) {
-                    System.out.println(resultSet.getString(1));
+                PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO AuthToken (Token, Email, TimeCreated)" +
+                        "VALUES(?,?,?);");
+                preparedStatement.setString(1,generatedToken);
+                preparedStatement.setString(2, request.getEmail());
+                preparedStatement.setTime(3, new Time(System.currentTimeMillis()));
+                int result = preparedStatement.executeUpdate();
+                if (result != 0) {
+                    return true;
                 }
+                else {
+                    return false;
+                }
+//                ResultSet resultSet = statement.executeQuery("Select * FROM AuthToken;");
+//                while(resultSet.next()) {
+//                    System.out.println(resultSet.getString(1));
+//                }
             }
+            return false;
         }
 
 }
